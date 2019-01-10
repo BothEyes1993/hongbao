e8a2251743fce3ad4a04d25705f45908c349dde1
BothEyes <1993046671@qq.com>
2019-01-11 03:27:24 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hongbao
-一个基于vue用纯sass写的红包雨效果，仅供参考
+> A Vue.js project
+一个基于VUE用纯sass写的红包雨效果，仅供参考。
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/2019011103252220.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3UwMTA2MzMyNjY=,size_16,color_FFFFFF,t_70)
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="red-package-contain">
+    <img src="@/assets/hongbao.png" :class="getClass()" :style="getStyle()" v-for="item in count" :key="item" @webkitAnimationIteration='iterationEvent(item, $event)'>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: 30
+    },
+    minSize: {
+      type: Number,
+      default: 30
+    },
+    maxSize: {
+      type: Number,
+      default: 100
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    iterationEvent (item, $event) {
+      $event.target.style.cssText = this.getStyle()
+    },
+    getClass () {
+      return `hb-item hbsd-${Math.floor(Math.random() * 50 + 30)}`
+    },
+    getStyle () {
+      return `width:${Math.random() * (this.maxSize - this.minSize) + this.minSize}px;left:${Math.random() * 74 + 10}%`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hb-item{
+  position: absolute;
+  top: 0;
+  z-index: 30000;
+  cursor: pointer;
+}
+/////////////////////////////////
+@keyframes startHB {
+  0% {
+    transform: translateY(-300px);
+    -ms-transform:translateY(-300px);
+    -webkit-transform:translateY(-300px);
+  }
+  100% {
+    transform: translateY(100vh);
+    -ms-transform:translateY(100vh);
+    -webkit-transform:translateY(100vh);
+  }
+}
+@keyframes chandou{
+    0%{margin-left:-120px}
+    50%{margin-left:0px}
+    100%{margin-left:120px}
+}
+
+$total: 100;
+@for $i from 1 through $total {
+  .hbsd-#{$i}{
+      animation: startHB #{$i/20}s linear infinite,chandou #{$i/20}s infinite linear alternate both;
+      -ms-animation:startHB #{$i/20}s linear infinite,chandou #{$i/20}s infinite linear alternate both;
+      -webkit-animation:startHB #{$i/20}s linear infinite,chandou #{$i/20}s infinite linear alternate both;
+    }
+}
+/////////////////////////////////
+</style>
